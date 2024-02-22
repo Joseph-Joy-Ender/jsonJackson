e9422372898628453a79cb5ed911e52d2aaf6b07
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-02-22 16:08:16 +0100

    json jackson

--- src/json/serialization/Product.java
+++ src/json/serialization/Product.java
@@ -1,2 +1,66 @@
-package json.serialization;public class Product {
+package json.serialization;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Product {
+
+    private String productName;
+    private BigDecimal price;
+    @JsonProperty("created_at")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdAt;
+
+    public Product(String productName, BigDecimal price, LocalDateTime createdAt) {
+        this.productName = productName;
+        this.price = price;
+        this.createdAt = createdAt;
+    }
+
+
+    public Product() {
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                       Product
+                productName: %s
+                price: %s
+                createdAt: %s
+                """, productName, price, createdAt);
+
+    }
 }
