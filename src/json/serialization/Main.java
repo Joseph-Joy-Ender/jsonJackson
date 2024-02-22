5d454e8abe31c8bfdde17ef073980dd73134cb7b
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-02-22 16:07:14 +0100

    first commit

---
+++ src/json/serialization/Main.java
@@ -0,0 +1,26 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import json.serialization.Product;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product product =
+                new Product("Chocolate",
+                        BigDecimal.TEN,
+                        LocalDateTime.of(2024, 2, 18,
+                10, 0, 0));
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String json = mapper.writeValueAsString(product);
+            System.out.println(json);
+
+        }catch (JsonProcessingException exception){
+            System.err.println("Error: " + exception.getMessage());
+        }
+
+    }
+}
